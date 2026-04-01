26db9dbfbbf662a5506a2aab938db5f748a7a632
Lamee <Lamee@Mac-mini.local>
2026-04-01 21:55:53 +0700

    update code

--- services/supabase_client.py
+++ services/supabase_client.py
@@ -1,7 +1,7 @@
 from supabase import create_client
 import os
 
-url = os.environ.get("https://syqblzpshdoquyffmwwq.supabase.co")
-key = os.environ.get("sb_publishable_bRMs8uZNdWXzlpcP0h21vw_vRUMF4RM")
+url = os.environ.get("SUPABASE_URL")
+key = os.environ.get("SUPABASE_KEY")
 
 supabase = create_client(url, key)
